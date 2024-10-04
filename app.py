21fdff2e3c18bc6246e17ccbe455a0f3312948cb
monalisah33 <monalisahfadzain@gmail.com>
2024-10-04 17:12:41 +0000

    First commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+# SQLite configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # Create an SQLite database named app.db
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable track modifications for performance
+
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def main():
@@ -12,3 +20,13 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
+    class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+
+# Create the database tables
+with app.app_context():
+    db.create_all()  # Creates the SQLite database and tables
+
